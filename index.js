c23eb9589096dbbe005cef6827181b5382790b66
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-04-22 01:21:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+function distanceTravelledInFeet(block1, block2){
+const inFeet = (block2 - block1)*264;
+return inFeet;
+}
+
+function distanceFromHqInBlocks(distance){
+const distanceInBlocks = distance/264;
+return distanceInBlocks;
+}
