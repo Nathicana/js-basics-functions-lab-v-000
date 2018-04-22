26184e72b9304cd377d867df17dc5372826500e6
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-04-22 01:34:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 // Code your solution in this file!
 function distanceTravelledInFeet(block1, block2){
+  if (block2 > block1) {
 const inFeet = (block2 - block1)*264;
+} else if (block1 < block2) {
+    const inFeet = (block1 - block2)*264;
+  }
+
 return inFeet;
 }
+
 
 function distanceFromHqInBlocks(distance){
 const distanceInBlocks = distance/264;
 return distanceInBlocks;
+}
+
+function distanceFromHqInFeet(){
+  distanceFromHqInBlocks();
 }
